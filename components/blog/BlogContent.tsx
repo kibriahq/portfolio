75e567fcd77233dc4d4d128328a7edf1e204f954
kibria <mdkibria.dev@gmail.com>
2026-08-14 22:55:51 +0600

    change isomorphic-dompurify with dompurify

--- components/blog/BlogContent.tsx
+++ components/blog/BlogContent.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useMemo } from "react";
-import DOMPurify from "isomorphic-dompurify";
+import DOMPurify from "dompurify";
 
 type BlogContentProps = {
   /** Raw HTML string from a Tiptap editor. Sanitized before rendering. */
